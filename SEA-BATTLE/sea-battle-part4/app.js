ba1f7008baaae003a44b1ad87003a2da7d4a6a83
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-03-15 01:56:15 +0100

    Delete SEA-BATTLE/sea-battle-part4 directory

--- SEA-BATTLE/sea-battle-part4/app.js
+++
@@ -1,9 +0,0 @@
-let seaDiv = document.querySelector('.sea')
-
-for(let i=0; 1<= 9; i++){
-  seaDiv.innerHTML += `
-          <div class="layer"> <!-- layer ${i} -->
-          <div class="wave"></div>
-        </div> 
-        `
-}
